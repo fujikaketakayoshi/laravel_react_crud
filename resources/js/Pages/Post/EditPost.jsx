8931efe11fe82101e3a39f46885751d9392cd1ef
fjkk <fujikake.takayoshi@gmail.com>
2024-10-23 23:52:08 +0900

    crudのcrdまで

---
+++ resources/js/Pages/Post/EditPost.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import toastr from "toastr";
+
+const EditPost = () => {
+	
+	return (
+		<>
+
+		</>
+	);
+};
+
+export default EditPost;
